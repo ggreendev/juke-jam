0e71c76204762891ec57da31e76bfa6798861b8e
vinceajcs <hvincentstephen@gmail.com>
2018-09-03 21:59:25 -0700

    add react routes

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Host from './Host';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// ReactDOM.render(
+//   <App/>,
+//   document.getElementById('root'));
+
+ReactDOM.render(
+  <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/host" component={Host} />
+      </div>
+  </Router>,
+  document.getElementById('root'));
